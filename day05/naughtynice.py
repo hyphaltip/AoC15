698d0e6dea40b873df24f7d165cf33b125deda0f
Jason Stajich <jason.stajich@ucr.edu>
2015-12-22 00:41:15 -0500

    part 1 solved

--- day05/naughtynice.py
+++ day05/naughtynice.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python
-
+import re
 # It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
 # It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
 
 # It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
 
-read = open("input2","r")
+read = open("input","r")
 
-vowelstr = "aeiou"
-vowels = {}
-for ch in vowelstr:
-    vowels[ch] = 1 
+vowelpattern = re.compile("[aeiou]")
+rpat  = re.compile(r"([a-z])\1")
+naughty = re.compile('ab|cd|pq|xy')
+
+nice_count = 0
 for line in read:
-    line.rstrip() # get rid of newline
-    vowelct = 0
-    for ch in line:
-        if vowels[ch]:
-            vowelct += 1
-    if vowelct >= 3:
-        print line, "vowels ok"
+    line = line.rstrip() # get rid of newline
+    if naughty.search(line):
+  #      print "line",line, "is naughty by pattern"
+        continue
+   # print "line is ",line
+    vowels = vowelpattern.findall(line)
+    #print "vowels are ",str(vowels)
+    m = rpat.search(line)
+
+#    if m:
+#        print "match group is ", m.group(1)
+
+    if len(vowels) >= 3 and m:
+#       print line, "vowels ok; letter_run is"
+       nice_count += 1
+
+print "nice count is",nice_count
